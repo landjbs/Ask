28531508314ad8df3b98b5b06ae90f2c2582cf83
Landon Smith <landjbs@gmail.com>
2019-11-14 15:55:59 -0500

    Question v1 imp

--- structs.py
+++ structs.py
@@ -1,6 +1,10 @@
 class Question(object):
     ''' Single question about a doc stores '''
-    
+    def __init__(self, questionId, text, span, is_impossible):
+        self.questionId = questionId
+        self.text = text
+        self.span = span
+        self.is_impossible = is_impossible
 
 class Document(object):
     ''' Single document from any QA dataset '''
