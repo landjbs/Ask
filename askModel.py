8b236add6e5bd088c7f18d91742193433eaa556c
Landon Smith <landjbs@gmail.com>
2019-11-14 16:13:57 -0500

    ask model header

--- askModel.py
+++ askModel.py
@@ -3,6 +3,6 @@ STEPS:
     - Read text with GRU -> Convo -> Dense (text len) to select
       interesting spans.
     - Use inverse of Answer model to estimate CLS vector of question related
-      to selected span.
-    - 
+      to selected span. Inverse model will take
+    - Use
 '''
