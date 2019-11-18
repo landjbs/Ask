8d0f1e3f3c25e91f4643fbe2211058385b0868a5
Landon Smith <landjbs@gmail.com>
2019-11-18 17:20:22 -0500

    generator search table

--- structs.py
+++ structs.py
@@ -50,6 +50,46 @@ class SearchTable(object):
         self.initialized = True
         return True
 
+    def question_generator(questions, textWords):
+        for q in questions:
+            if q['is_impossible']:
+                continue
+            try:
+                answerList = q['answers']
+            except KeyError:
+                answerList = []
+            if (answerList == []):
+                answerList = q['plausible_answers']
+                if (answerList == []):
+                    continue
+            # focuses only on the first answer of answer list
+            answer = answerList[0]
+            answerText = answer['text']
+            answerWords = tokenize(answerText)
+            answerStart = answerWords[0]
+            answerLen = len(answerWords)
+            span = None
+            for loc, textWord in enumerate(textWords):
+                if (textWord==answerStart):
+                    if (textWords[loc:(loc+answerLen)] == answerWords):
+                        span = (loc, loc+answerLen)
+                        break
+            if not span:
+                continue
+            yield Question(q['id'], q['question'], answerText, span)
+    #
+    def document_generator(category):
+            title = category['title']
+            for docId, document in enumerate(category['paragraphs']):
+                text = document['context']
+                textWords = tokenize(text)
+                questions = document['qas']
+                questionIdx = {i : qObj for i, qObj
+                                in enumerate(question_generator(questions,
+                                                                textWords))}
+                yield Document(docId, title, text, questionIdx)
+
+
     def build(self, squadPath):
         ''' Builds SearchTable from squad file under squadPath'''
         with open(squadPath, 'r') as squadFile:
