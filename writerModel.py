0b421f9eb46d78ca262e261d61a54834b8e3913e
Landon Smith <landjbs@gmail.com>
2019-11-19 17:01:21 -0500

    gen loop v1

--- writerModel.py
+++ writerModel.py
@@ -5,5 +5,14 @@ generation.
 
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
+# init models and tokenizer
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model.eval()
+
+while True:
+    text = input('text: ')
+    context_tokens = tokenizer.encode(text, add_special_tokens=False)
+    print(context_tokens)
+
+# def generate(text):
