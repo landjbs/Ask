e78de19ef37d5fb499e69bd7cb2f6c1ad34322b6
Landon Smith <landjbs@gmail.com>
2019-11-20 16:01:11 -0500

    data_generator text processing

--- clsTest.py
+++ clsTest.py
@@ -5,23 +5,35 @@ from transformers import GPT2Tokenizer, GPT2DoubleHeadsModel
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2DoubleHeadsModel.from_pretrained('gpt2')
+tokenizer.add_special_tokens({'cls_token': '[CLS]'})
+model.resize_token_embeddings(len(tokenizer))
 
 def data_generator(folderPath='data/aclImdb/test'):
     negFolder = f'{folderPath}/neg'
     posFolder = f'{folderPath}/pos'
-    for posFile, negFile in zip(listdir(posFolder), listdir(negFolder)):
-        
+    for posPath, negPath in tqdm(zip(listdir(posFolder), listdir(negFolder))):
+        with open(f'{posFolder}/{posPath}', 'r') as posFile:
+            posText = posFile.read()
+        with open(f'{negFolder}/{negPath}', 'r') as negFile:
+            negText = negFile.read()
+    posText = '[CLS]' + posText
+    negText = '[CLS]' + negText
+    cls_token_location = [0, 0]
+    input_ids = torch.tensor(encoded_choices).unsqueeze(0)
+    
 
 
 # Add a [CLS] to the vocabulary (we should train it also!)
-tokenizer.add_special_tokens({'cls_token': '[CLS]'})
-model.resize_token_embeddings(len(tokenizer))  # Update the model embeddings with the new vocabulary size
-print(tokenizer.cls_token_id, len(tokenizer))  # The newly token the last token of the vocabulary
 
+  # Update the model embeddings with the new vocabulary size
+print(tokenizer.cls_token_id, len(tokenizer))  # The newly token the last token of the vocabulary
+choices = ['hi [CLS]', 'yo what up [CLS]']
 encoded_choices = [tokenizer.encode(s) for s in choices]
+print(encoded_choices)
 cls_token_location = [tokens.index(tokenizer.cls_token_id) for tokens in encoded_choices]
+print(cls_token_location)
 input_ids = torch.tensor(encoded_choices).unsqueeze(0)  # Batch size: 1, number of choices: 2
+print(input_ids)
 mc_token_ids = torch.tensor([cls_token_location])  # Batch size: 1
 outputs = model(input_ids, mc_token_ids=mc_token_ids)
-print(outputs)
 lm_prediction_scores, mc_prediction_scores = outputs[:2]
