cbb01152fabf06b3e72e14a9a28e77edc87063e4
Landon Smith <landjbs@gmail.com>
2019-11-27 15:47:50 -0500

    span debugging

--- reader.py
+++ reader.py
@@ -6,6 +6,7 @@ from structs import SearchTable
 
 
 x = SearchTable('data/inData/dev-v2.0.json')
-for z in x.iter_docs():
-    for q in z.iter_questions():
-        pass
+
+# for z in x.iter_docs():
+#     for q in z.iter_questions():
+#         pass
