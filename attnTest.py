bd92f0235c80fda670a0791345e062945ae3589d
Landon Smith <landjbs@gmail.com>
2019-12-18 14:39:04 -0500

    lang undep

--- attnTest.py
+++ attnTest.py
@@ -14,37 +14,37 @@ from torch.cuda import is_available as gpu_available
 
 device = torch.device("cuda" if gpu_available() else "cpu")
 
-# class Lang(object):
-#     def __init__(self, inVocab, outVocab):
-#         assert len(set(inVocab)) == len(set(outVocab)), 'lens must be same'
-#         self.inIdx = {i : word for i, word in enumerate(inVocab)}
-#         self.inRev = {word : i for i, word in self.inIdx.items()}
-#         self.outIdx = {i : word for i, word in enumerate(outVocab)}
-#         self.outRev = {word : i for i, word in self.outIdx.items()}
-#         self.voxLen = len(self.outIdx)
-#         # build translation schema
-#         self.transIdx = {inW : outW for inW, outW in zip(inVocab, outVocab)}
-#         self.transRev = {outW : inW for inW, outW in self.transIdx.items()}
-#
-#     def gen_seqPair(self, inLen):
-#         ''' gen pair of context, question tuple '''
-#         inIds = np.random.randint(0, self.voxLen, size=inLen)
-#         inWords = [self.inIdx[id] for id in inIds]
-#         outWords = [self.transIdx[w] for w in inWords]
-#         outIds = [self.outRev[w] for w in outWords]
-#         inTensor = torch.tensor(inIds, dtype=torch.long,
-#                                 device=device).view(-1, 1)
-#         outTensor = torch.tensor(outIds, dtype=torch.long,
-#                                 device=device).view(-1, 1)
-#         return (inTensor, outTensor)
-#
-#     def str_seq(self, seq, n):
-#         if n==0:
-#             return ' '.join((self.inIdx[id] for id in seq))
-#         elif n==1:
-#             return ' '.join((self.outIdx[id] for id in seq))
-#         else:
-#             raise ValueError('n must be in {0, 1}')
+class Lang(object):
+    def __init__(self, inVocab, outVocab):
+        assert len(set(inVocab)) == len(set(outVocab)), 'lens must be same'
+        self.inIdx = {i : word for i, word in enumerate(inVocab)}
+        self.inRev = {word : i for i, word in self.inIdx.items()}
+        self.outIdx = {i : word for i, word in enumerate(outVocab)}
+        self.outRev = {word : i for i, word in self.outIdx.items()}
+        self.voxLen = len(self.outIdx)
+        # build translation schema
+        self.transIdx = {inW : outW for inW, outW in zip(inVocab, outVocab)}
+        self.transRev = {outW : inW for inW, outW in self.transIdx.items()}
+
+    def gen_seqPair(self, inLen):
+        ''' gen pair of context, question tuple '''
+        inIds = np.random.randint(0, self.voxLen, size=inLen)
+        inWords = [self.inIdx[id] for id in inIds]
+        outWords = [self.transIdx[w] for w in inWords]
+        outIds = [self.outRev[w] for w in outWords]
+        inTensor = torch.tensor(inIds, dtype=torch.long,
+                                device=device).view(-1, 1)
+        outTensor = torch.tensor(outIds, dtype=torch.long,
+                                device=device).view(-1, 1)
+        return (inTensor, outTensor)
+
+    def str_seq(self, seq, n):
+        if n==0:
+            return ' '.join((self.inIdx[id] for id in seq))
+        elif n==1:
+            return ' '.join((self.outIdx[id] for id in seq))
+        else:
+            raise ValueError('n must be in {0, 1}')
 #
 #
 # inVocab = ['hi', 'you', 'are', 'cool']
@@ -107,7 +107,10 @@ class AttnDecoderRNN(nn.Module):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
 
-class AttnModel(object)
+class AttnModel(object):
+    def __init__(self, lang):
+        self.lang = lang
+        self.hiddenDim = lang.
 
 
 
