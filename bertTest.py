f7c979873e9be266d57d67baafc040eb000504dc
Landon Smith <landjbs@gmail.com>
2019-12-18 14:29:27 -0500

    bert test init

---
+++ bertTest.py
@@ -0,0 +1,31 @@
+import torch
+import numpy as np
+from torch import nn
+from tqdm import tqdm, trange
+from termcolor import colored
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torch.cuda import is_available as gpu_available
+
+from bert_serving.client import BertClient
+
+
+
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
