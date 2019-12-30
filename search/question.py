b943198ff7924145b2e7fd730cf4c904edc8e56e
Landon Smith <landjbs@gmail.com>
2019-12-30 13:52:23 -0700

    question v1 imp

--- search/question.py
+++ search/question.py
@@ -6,5 +6,13 @@ class Question(object):
                 from doc.questions.
         text:   Id-encoded text of question.
         span:   Tuple of (start, end) span of question in document.
-        asker:  Id of question asker.
+        asker:  String id of question asker.
     '''
+    def __init__(self, id, text, span, asker):
+        self.id = id
+        self.text = text
+        self.span = span
+        self.asker = asker
+
+    def __str__(self):
+        return f'<Question | {self.text}>'
