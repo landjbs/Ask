a85aa4bde53324a1bc3e956539a202589b3b3964
Landon Smith <landjbs@gmail.com>
2019-12-30 13:22:44 -0700

    base imports

---
+++ models/base.py
@@ -0,0 +1,13 @@
+'''
+Base models to be added or adapted for higher level Ask and Answer models.
+'''
+
+import torch
+import numpy as np
+from torch import nn
+from tqdm import tqdm, trange
+from termcolor import colored
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torch.cuda import is_available as gpu_available
