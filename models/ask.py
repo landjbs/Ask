8236cbdb6c5673d46ff37b1eb4358cebb05fe4d2
Landon Smith <landjbs@gmail.com>
2020-01-02 11:51:00 -0500

    decoder prog

--- models/ask.py
+++ models/ask.py
@@ -4,8 +4,14 @@ class Ask_Decoder(nn.Module):
     '''
     Decoder model that uses encoder transpose and attn rnn to estimate
     '''
-    def __init__(self):
-        pass
+    def __init__(self, hiddenDim, layerNum):
+        super(Ask_Decoder, self).__init__()
+        # attributes
+        self.hiddenDim = hiddenDim
+        self.layerNum = layerNum
+        # layers
+        self.rnn = nn.GRU(input_size=hiddenDim)
+
 
 class Ask(object):
     def __init__(self, searchTable, maxLen):
