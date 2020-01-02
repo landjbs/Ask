a50ca35284f64146d8e932a432b969ea905ae691
Landon Smith <landjbs@gmail.com>
2020-01-02 12:02:40 -0500

    decoder prog

--- models/ask.py
+++ models/ask.py
@@ -4,12 +4,17 @@ class Ask_Decoder(nn.Module):
     '''
     Decoder model that uses encoder transpose and attn rnn to estimate
     '''
-    def __init__(self, hiddenDim, layerNum):
+    def __init__(self, hiddenDim, layerNum, maxLen, dropP):
         super(Ask_Decoder, self).__init__()
         # attributes
         self.hiddenDim = hiddenDim
         self.layerNum = layerNum
+        self.dropP = dropP
         # layers
+        self.attn = nn.Linear(in_features=(2*hiddenDim),
+                              out_dim=maxLen)
+        self.attn_combine = nn.Linear(in_features=(2*hiddenDim),
+                                      out_features=hiddenDim)
         self.relu = F.relu()
         self.rnn = nn.GRU(input_size=hiddenDim,
                           hidden_size=hiddenDim,
@@ -17,10 +22,13 @@ class Ask_Decoder(nn.Module):
                           batch_first=True)
         self.dense = nn.Linear(in_features=hiddenDim,
                                out_features=outDim)
-        self.softmax = nn.Softmax(dim=1)
+        self.catSoft = nn.Softmax(dim=1)
 
-    def forward(self, prevEmbedding, hidden):
-        prevEmbedding = self.relu(prevEmbedding.view(1, 1, -1))
+    def forward(self, prevEmbedding, hidden, encoderOuts):
+        embed = self.relu(prevEmbedding.view(1, 1, -1))
+        C = torch.cat((embed[0], hidden[0]), axis=1)
+        W = self.attn(torch)
+        W = self.
 
 
 class Ask(object):
