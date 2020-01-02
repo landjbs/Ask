16e0ec352e7952c4562cd738ee82eee98034aeb1
Landon Smith <landjbs@gmail.com>
2020-01-01 20:25:19 -0500

    Ask_Decoder

--- models/ask.py
+++ models/ask.py
@@ -1,5 +1,11 @@
 from base import *
 
+class Ask_Decoder(nn.Module):
+    '''
+    Decoder model that uses encoder transpose and attn rnn to estimate
+    '''
+    def __init__(self):
+        pass
 
 class Ask(object):
     def __init__(self, searchTable, maxLen):
