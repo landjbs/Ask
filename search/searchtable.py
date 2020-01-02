8cc9ebeae1c708a25d198140418116c57115dde2
Landon Smith <landjbs@gmail.com>
2020-01-01 20:18:34 -0500

    search_table prog

---
+++ search/searchtable.py
@@ -0,0 +1,21 @@
+class SearchTable(object):
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.keywordIdx = {}
+        self.clusterIdx = {}
+        self.idIdx = {}
+        self.pageNum = 0
+
+    def __str__(self):
+        return f'<SearchTable | pageNum={self.pageNum}>'
+
+    def add_document(self, document):
+        ''' Adds single document to kewordIdx, clusterIdx, and idIdx '''
+        self.add_keyword_doc(document)
+        self.add_cluster_doc(document)
+        self.add_id_doc(document)
+
+    def add_keyword_doc(self, document):
+        tokenScores = document.
+
+    def load_squad(self, )
--- testing/dataGen.py
+++ testing/dataGen.py
@@ -2,14 +2,14 @@ from search.document import Document
 from search.question import Question
 
 
-nounList = [chr(i) for i in range(65, 91)]
-adjList = [chr(i) for i in range(97, 123)]
-
-
-def gen_example(i, noun, adj):
-    ''' Gens single document with a question '''
-    c = f'{noun} is very {adj}.'
-    q = f'what is {noun}.'
-    ques = Question(0, q, (2, 3))
-    doc = Document(i, {}, {0: ques}, [], None, c)
-    return doc
+# nounList = [chr(i) for i in range(65, 91)]
+# adjList = [chr(i) for i in range(97, 123)]
+#
+#
+# def gen_example(i, noun, adj):
+#     ''' Gens single document with a question '''
+#     c = f'{noun} is very {adj}.'
+#     q = f'what is {noun}.'
+#     ques = Question(0, q, (2, 3))
+#     doc = Document(i, {}, {0: ques}, [], None, c)
+#     return doc
